4ca3d5b63fa7be3d7642ecd546069c36cd08661c
Michael <halasnik@bu.edu>
2022-07-31 13:46:50 -0400

    Added Timer and submissions category

--- cogs/xquester.py
+++ cogs/xquester.py
@@ -4,6 +4,7 @@ from click import command
 from discord.ext import commands
 import discord
 import random
+import asyncio
 
 class Xquester(commands.Cog):
     def __init__(self, client, name='Xquester Cog'):
@@ -16,6 +17,7 @@ class Xquester(commands.Cog):
         self.limit = -1
         self.player_role = None
         self.category = None
+        self.submissions = None
         self.announcements = None
         self.register_channel = None
         self.rooms_created = False
@@ -27,6 +29,7 @@ class Xquester(commands.Cog):
         self.role_rooms = {}
         # Role -> [Players]
         self.player_room_roles = {}
+        self.player_votes = {}
 
     @commands.command()
     async def create_game(self, ctx, limit):
@@ -38,29 +41,39 @@ class Xquester(commands.Cog):
         self.player_role = await guild.create_role(name="Player")
 
         self.category = await guild.create_category("Xquester")
+        self.submissions = await guild.create_category("Submissions")
         self.announcements = await guild.create_text_channel("announcements", category=self.category)
         self.register_channel = await guild.create_text_channel("register", category=self.category)
 
         await self.announcements.set_permissions(ctx.guild.default_role, read_messages=True, send_messages=False)
         await self.announcements.set_permissions(self.player_role, read_messages=True, send_messages=False)
         await self.register_channel.set_permissions(self.player_role, send_messages=True, read_messages=True)
+        await self.submissions.set_permissions(ctx.guild.default_role, read_messages=False, send_messages=False)
+        await self.submissions.set_permissions(self.player_role, read_messages=False, send_messages=False)
 
     @commands.command()
     async def register(self, ctx):
         player = ctx.message.author
+        guild = ctx.guild
 
         if self.player_count < self.limit and self.game_started and ctx.channel.id == self.register_channel.id:
             await player.add_roles(self.player_role)
             self.players.append(player)
             self.player_count += 1
+
+            submissions_channel = await guild.create_text_channel(player.name + "-submissions", category=self.submissions)
+            await submissions_channel.set_permissions(player, read_messages=True, send_messages=True) 
             await ctx.send(ctx.message.author.mention + ", you have been registered!")
         elif not self.game_started:
             await ctx.send("The game hasn't even started yet!")
         elif ctx.channel.id != self.register_channel.id:
             await ctx.send("You cannot register here.")
         else:
-            await self.register_channel.set_permissions(self.player_role, send_messages=False)
             await ctx.send("Registration has ended. Try again next time!")
+
+        if self.player_count < self.limit:
+            await ctx.send("**Registration has ended. Try again next time!**")
+            await self.register_channel.set_permissions(self.player_role, send_messages=False)
 
     @commands.command()
     async def create_rooms(self, ctx, room_count, room_capacity):
@@ -122,7 +135,6 @@ class Xquester(commands.Cog):
                 if self.rooms_created and self.rooms[dest_room] < self.room_capacity:
                     await player.remove_roles(origin_role)
                     await player.add_roles(dest_role)
-                    print(self.player_room_roles)
                     self.player_room_roles[origin_role].remove(player)
                     self.player_room_roles[dest_role].append(player)
                     self.rooms[origin_room] -= 1
@@ -170,7 +182,65 @@ class Xquester(commands.Cog):
 
     @commands.command()
     async def end_rooms(self, ctx):
+        if self.rooms_created:
+            for room in self.rooms.keys():
+                await room.delete()
+
+            for role in self.room_roles.values():
+                await role.delete()
+
+            self.rooms_created = False
+            self.rooms = {}
+            self.room_roles = {}
+            self.role_rooms = {}
+            self.player_room_roles = {}
+
+    @commands.command()
+    async def start_timer(self, ctx, time_input):
+        try:
+            try:
+                time = int(time_input)
+            except:
+                convertTimeList = {'s':1, 'm':60, 'h':3600, 'd':86400, 'S':1, 'M':60, 'H':3600, 'D':86400}
+                time = int(time_input[:-1]) * convertTimeList[time_input[-1]]
+            if time > 86400:
+                await ctx.send("Please enter a value less than 24 hours.")
+                return
+            if time <= 0:
+                await ctx.send("Please enter a non-negative value.")
+                return
+            if time >= 3600:
+                message = await self.announcements.send(f"Timer: {time//3600}:{time%3600//60}:{time%60}")
+            elif time >= 60:
+                message = await self.announcements.send(f"Timer: {time//60}:{time%60}")
+            elif time < 60:
+                message = await self.announcements.send(f"Timer: {time}")
+            while True:
+                try:
+                    await asyncio.sleep(1)
+                    time -= 1
+                    if time >= 3600:
+                        await message.edit(content=f"Timer: {time//3600}:{time %3600//60}:{time%60}")
+                    elif time >= 60:
+                        await message.edit(content=f"Timer: {time//60}:{time%60}")
+                    elif time < 60:
+                        await message.edit(content=f"Timer: {time}")
+                    if time <= 0:
+                        await message.edit(content=f"Timer: {time}")
+                        await self.announcements.send(f"{self.player_role.mention}s, the countdown has ended! Rooms are now closing.")
+                        await asyncio.sleep(10)
+                        await self.end_rooms(ctx)
+                        break
+                except:
+                    break
+        except:
+            await ctx.send(f"Alright, first you gotta let me know how I\'m gonna time **{time_input}**....")
+
+    @commands.command()
+    async def vote(self, ctx, name):
         pass
+
+    
 
 def setup(client):
 	client.add_cog(Xquester(client))
